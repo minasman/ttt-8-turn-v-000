87247401f2acf344c001038a222bc34ff5ea28da
elreymcd@yahoo.com <elreymcd@yahoo.com>
2018-05-12 12:59:11 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,27 +1,46 @@
-def turn(board)
-  puts "Please enter 1-9"
-end
-
-
 def display_board(board)
-  puts "#{board[0]}|#{board[1]}|#{board[2]}"
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
   puts "-----------"
-  puts "#{board[3]}|#{board[4]}|#{board[5]}"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
   puts "-----------"
-  puts "#{board[6]}|#{board[7]}|#{board[8]}"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
-def valid_move?(board, index, user)
-  if index >= 0 && index <= 8
-    def position_taken?(board,index)
-      if board[index] == " " || board[index] == "" or board[index] == NIL
-        return TRUE
-      elsif
-        return FALSE
-      end
-    end
-  position_taken(board,index)
-  elsif
-    return "Not a valid move"
+def turn(board)
+  puts "Please enter 1-9:"
+  user_input = gets.strip
+  user_move = input_to_index(user_input)
+   if !valid_move?(board, user_move)
+     turn(board)
+   else
+     move(board,user_move)
+     display_board(board)
+   end
+end
+
+def move(board, index, user = "X")
+  return board[index] = user
+end
+
+
+  
+def input_to_index(user_input)
+  return user_input.to_i - 1
+end
+
+def valid_move?(board, index)
+  if index < 0 || index > 8
+    return FALSE
+  elsif board[index] == " " || board[index] == "" || board[index] == NIL
+    return TRUE
   end
 end
+
+  
+  def position_taken?(board, index)
+    if board[index] == " " || board[index] == "" || board[index] == nil
+      return FALSE
+    else
+      return TRUE
+    end
+  end
